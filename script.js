cb22accef5c3578498b98db70720576328d79b8d
deepeshyadavrd <raoshsbh6@gmail.com>
2024-11-21 13:26:23 +0530

    navigation buttons added with css, pagination tabs move auto on small screen

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const sliderTabs = document.querySelectorAll('.slider-tab');
 const sliderIndicator = document.querySelector('.slider-indicator');
+const sliderControls = document.querySelector('.slider-controls');
 
 // update indicator height and width
 const updateIndicator = (tab, index) => {
     sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
     sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+
+    const scrollLeft = sliderTabs[index].offsetLeft - sliderControls.offsetWidth / 2 + sliderTabs[index].offsetWidth / 2;
+    sliderControls.scrollTo({ left: scrollLeft, behavior: "smooth"});
 }
 
 const swiper = new Swiper(".slider-container",{
     effect: "fade",
     speed: 1300,
     // autoplay: { delay: 4000 }
+    navigation: {
+        prevEl: "#slide-prev",
+        nextEl: "#slide-next"
+    },
     on: {
         slideChange: () => {
             const currentTabIndex = [...sliderTabs].indexOf(sliderTabs[swiper.activeIndex]);
